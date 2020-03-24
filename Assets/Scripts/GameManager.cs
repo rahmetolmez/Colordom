5c57278032f8f6f4c07be997dfe16158305f500e
Rahmet Ali Ölmez <rahmetolmez@gmail.com>
2020-03-24 19:19:58 +0300

    Menu implementation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isGameOver = false;
+    public float delay = 1f;
+    public GameObject levelClearUI;
+
+    public void LevelClear()
+    {
+        levelClearUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (!isGameOver)
+        {
+            isGameOver = true;
+            Invoke("Restart", delay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
